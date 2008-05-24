be375a289beb122d383514ac8677664b47c5f51a
jdmp <jdmp@0b692bd1-164c-0410-85bb-a4b81a012900>
2008-05-24 12:22:19 +0000

    update

--- jdmp-core/trunk/src/test/java/org/jdmp/core/AllTests.java
+++ jdmp-core/trunk/src/test/java/org/jdmp/core/AllTests.java
@@ -2,15 +2,11 @@ package org.jdmp.core;
 
 import junit.framework.TestSuite;
 
+public class AllTests extends TestSuite {
 
-
-  public class AllTests extends TestSuite {
-
-    public static TestSuite suite(){
-      TestSuite suite=new TestSuite(AllTests.class.getName());
-      return suite;
-    }
+	public static TestSuite suite() {
+		TestSuite suite = new TestSuite(AllTests.class.getName());
+		return suite;
+	}
 
 }
-  
-
