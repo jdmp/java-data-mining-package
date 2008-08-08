b559a6ba0118444931151d272884bd50e36c8939
jdmp <jdmp@0b692bd1-164c-0410-85bb-a4b81a012900>
2008-08-08 11:12:58 +0000

    new jgroups version

--- jdmp-core/trunk/src/main/java/org/jdmp/core/variable/VariableFactory.java
+++ jdmp-core/trunk/src/main/java/org/jdmp/core/variable/VariableFactory.java
@@ -1,7 +1,10 @@
 package org.jdmp.core.variable;
 
+import java.lang.reflect.Constructor;
+
 import org.jdmp.core.matrix.wrappers.MatrixToMatrixListWrapper;
 import org.ujmp.core.Matrix;
+import org.ujmp.core.exceptions.MatrixException;
 
 public abstract class VariableFactory {
 
@@ -19,6 +22,16 @@ public abstract class VariableFactory {
 
 	public static final Variable singleMatrixVariable() {
 		return new SingleMatrixVariable();
+	}
+
+	public static final Variable sharedInstance(String label) {
+		try {
+			Class<?> c = Class.forName("org.jdmp.jgroups.ReplicatedVariable");
+			Constructor<?> con = c.getConstructor(String.class);
+			return (Variable) con.newInstance(label);
+		} catch (Exception e) {
+			throw new MatrixException(e);
+		}
 	}
 
 }
