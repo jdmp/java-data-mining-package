91e8f88541ba973ef2a01478571c5b3b8f9b9f25
jdmp <jdmp@0b692bd1-164c-0410-85bb-a4b81a012900>
2008-11-14 16:23:03 +0000

    mail moved from JDMP to UJMP

--- jdmp-complete/trunk/src/test/java/org/jdmp/complete/AllTests.java
+++ jdmp-complete/trunk/src/test/java/org/jdmp/complete/AllTests.java
@@ -30,7 +30,6 @@ public class AllTests extends TestSuite {
 	public static TestSuite suite() {
 		TestSuite suite = new TestSuite(AllTests.class.getName());
 		suite.addTest(org.ujmp.complete.AllTests.suite());
-		suite.addTest(org.jdmp.mail.AllTests.suite());
 		suite.addTest(org.jdmp.core.AllTests.suite());
 		suite.addTest(org.jdmp.ehcache.AllTests.suite());
 		suite.addTest(org.jdmp.gui.AllTests.suite());
