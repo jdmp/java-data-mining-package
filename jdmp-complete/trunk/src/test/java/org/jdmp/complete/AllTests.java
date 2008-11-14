81ad6b3911358c6b91831a7bae7e90786f9348a2
jdmp <jdmp@0b692bd1-164c-0410-85bb-a4b81a012900>
2008-11-14 15:28:01 +0000

    iText moved from JDMP to UJMP

--- jdmp-complete/trunk/src/test/java/org/jdmp/complete/AllTests.java
+++ jdmp-complete/trunk/src/test/java/org/jdmp/complete/AllTests.java
@@ -34,7 +34,6 @@ public class AllTests extends TestSuite {
 		suite.addTest(org.jdmp.core.AllTests.suite());
 		suite.addTest(org.jdmp.ehcache.AllTests.suite());
 		suite.addTest(org.jdmp.gui.AllTests.suite());
-		suite.addTest(org.jdmp.itext.AllTests.suite());
 		suite.addTest(org.jdmp.libsvm.AllTests.suite());
 		suite.addTest(org.jdmp.weka.AllTests.suite());
 		return suite;
