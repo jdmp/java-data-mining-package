16530d07016fcd1dd3f39c9b5bd6ff2d5bd9e125
jdmp <jdmp@0b692bd1-164c-0410-85bb-a4b81a012900>
2008-11-27 18:25:11 +0000

    new main method

--- jdmp-core/trunk/src/main/java/org/jdmp/core/Main.java
+++ jdmp-core/trunk/src/main/java/org/jdmp/core/Main.java
@@ -23,16 +23,132 @@
 
 package org.jdmp.core;
 
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.io.PrintStream;
+
+import org.jdmp.core.interpreter.Result;
+import org.jdmp.core.module.Module;
+import org.jdmp.core.module.ModuleFactory;
+
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to the Java Data Mining Package (JDMP)");
-		System.out.println();
-		System.out.println("JDMP is not a standalone program, but a Java library.");
-		System.out.println("You can use it for analyzing data in your own applications.");
-		System.out.println("If you would like to find out more, please take a look at the");
-		System.out.println("JDMP homepage at http://www.jdmp.org");
-		System.out.println();
+	protected static final PrintStream out = System.out;
+
+	protected static String script = null;
+	protected static String file = null;
+	protected static boolean textMode = true;
+
+	public static void main(String[] args) throws Exception {
+		evaluateParameters(args);
+		run();
+	}
+
+	protected static void evaluateParameters(String[] args) {
+		// parse command line
+
+		try {
+
+			for (int i = 0; i < args.length; i++) {
+
+				if (args[i].equals("-e") || args[i].equals("--execute")) {
+					script = args[++i];
+				} else if (args[i].equals("-f") || args[i].equals("--file")) {
+					file = args[++i];
+				} else if (args[i].equals("-g") || args[i].equals("--gui")) {
+					textMode = false;
+				} else if (args[i].equals("-t") || args[i].equals("--text")) {
+					textMode = true;
+				} else if (args[i].equals("-h") || args[i].equals("--help")) {
+					help();
+					System.exit(0);
+				}
+
+			}
+
+		} catch (Exception e) {
+			help();
+			System.exit(1);
+		}
+	}
+
+	private static void help() {
+		out.println();
+		out.println("JDMP supports the following command line options:");
+		out.println("    JDMP [-tgh] [-e command] [-f file]");
+		out.println();
+		out.println("    -e, --execute     executes a command at startup");
+		out.println("    -f, --file        executes the commands in the specifiedfile");
+		out.println("    -t, --text        starts JDMP in textmode");
+		out.println("    -g, --gui         starts JDMP in graphics mode");
+		out.println("    -h, --help        displays this help text");
+		out.println();
+		out.println();
+	}
+
+	public static void run() throws Exception {
+		out.println("/*");
+		out.println(" *       Welcome to the Java Data Mining Package (JDMP)");
+		out.println(" * Copyright (C) 2008 by Holger Arndt, A. Naegele and M. Bundschus");
+		out.println(" *");
+		out.println(" * JDMP is free software; you can redistribute it and/or modify");
+		out.println(" * it under the terms of the GNU Lesser General Public License as");
+		out.println(" * published by the Free Software Foundation; either version 2");
+		out.println(" * of the License, or (at your option) any later version.");
+		out.println(" *");
+		out.println(" * JDMP is distributed in the hope that it will be useful,");
+		out.println(" * but WITHOUT ANY WARRANTY; without even the implied warranty of");
+		out.println(" * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the");
+		out.println(" * GNU Lesser General Public License for more details.");
+		out.println(" *");
+		out.println(" * If you would like to find out more, please take a look at the");
+		out.println(" * homepage of JDMP at http://www.jdmp.org/");
+		out.println(" */");
+		out.println();
+		out.println();
+
+		Module module = ModuleFactory.emptyModule();
+
+		if (textMode == false) {
+			try {
+				Class.forName("org.jdmp.gui.JDMP");
+				module.showGUI();
+			} catch (Exception e) {
+				out.println("Graphics mode is not available, probably because jdmp-gui is not");
+				out.println("in the class path.");
+				out.println();
+			}
+		}
+
+		// read from standard input
+
+		LineNumberReader lr = new LineNumberReader(new InputStreamReader(System.in));
+
+		while (true) {
+
+			try {
+
+				out.print(">> ");
+
+				String line = lr.readLine();
+
+				if (line.startsWith(">> ")) {
+					line = line.substring(3);
+				}
+
+				if (!line.endsWith(";")) {
+					Result result = module.execute(line + ";");
+					out.println(result);
+				} else {
+					module.execute(line);
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+
 	}
 
 }
