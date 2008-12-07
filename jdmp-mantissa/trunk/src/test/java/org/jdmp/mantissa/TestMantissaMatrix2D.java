608b85a395562c261b68b0844c80f9ad76fbe3d6
jdmp <jdmp@0b692bd1-164c-0410-85bb-a4b81a012900>
2008-12-07 21:06:49 +0000

    mantissa added to jdmp

---
+++ jdmp-mantissa/trunk/src/test/java/org/jdmp/mantissa/TestMantissaMatrix2D.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2008 Holger Arndt, Andreas Naegele and Markus Bundschus
+ *
+ * This file is part of the Java Data Mining Package (JDMP).
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * JDMP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * JDMP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with JDMP; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package org.jdmp.mantissa;
+
+import org.jdmp.mantissa.MantissaMatrix2D;
+import org.ujmp.core.Matrix;
+import org.ujmp.core.exceptions.MatrixException;
+import org.ujmp.core.util.test.AbstractMatrixTest;
+
+public class TestMantissaMatrix2D extends AbstractMatrixTest {
+
+	@Override
+	public Matrix createMatrix(long... size) throws MatrixException {
+		return new MantissaMatrix2D(size);
+	}
+
+	@Override
+	public Matrix createMatrix(Matrix source) throws MatrixException {
+		return new MantissaMatrix2D(source);
+	}
+
+}
