57b0fe48a9b1170bff759b5cd7af00f8ad8a31ab
jdmp <jdmp@0b692bd1-164c-0410-85bb-a4b81a012900>
2009-03-02 20:51:16 +0000

    lucene added

--- jdmp-complete/trunk/src/test/java/org/jdmp/complete/AllTests.java
+++ jdmp-complete/trunk/src/test/java/org/jdmp/complete/AllTests.java
@@ -39,6 +39,7 @@ public class AllTests extends TestSuite {
 		suite.addTest(org.jdmp.mallet.AllTests.suite());
 		suite.addTest(org.jdmp.weka.AllTests.suite());
 		suite.addTest(org.jdmp.bsh.AllTests.suite());
+		suite.addTest(org.jdmp.lucene.AllTests.suite());
 		return suite;
 	}
 
