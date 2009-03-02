1b8fb22719f2bb96d512837a7b935de1fa5fdc1c
jdmp <jdmp@0b692bd1-164c-0410-85bb-a4b81a012900>
2009-03-02 20:50:40 +0000

    lucene added

---
+++ jdmp-lucene/trunk/src/test/java/org/jdmp/lucene/TestLuceneIndex.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2008-2009 Holger Arndt, A. Naegele and M. Bundschus
+ *
+ * This file is part of the Java Data Mining Package (JDMP).
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * JDMP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * JDMP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with JDMP; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package org.jdmp.lucene;
+
+import junit.framework.TestCase;
+
+import org.jdmp.core.dataset.DataSet;
+import org.jdmp.core.dataset.DataSetFactory;
+
+public class TestLuceneIndex extends TestCase {
+
+	public void testExampleSearch() throws Exception {
+		DataSet ds = DataSetFactory.IRIS();
+		LuceneIndex index = new LuceneIndex();
+		index.add(ds);
+		DataSet result = index.search("setosa");
+		assertEquals(50, result.getSamples().getSize());
+	}
+
+}
