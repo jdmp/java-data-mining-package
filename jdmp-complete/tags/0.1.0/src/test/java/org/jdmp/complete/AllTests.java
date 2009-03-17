e5c17eb656646db58880aea012c397dc59981aa1
jdmp <jdmp@0b692bd1-164c-0410-85bb-a4b81a012900>
2009-03-17 22:25:49 +0000

    version tag created

---
+++ jdmp-complete/tags/0.1.0/src/test/java/org/jdmp/complete/AllTests.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2008-2009 Holger Arndt, A. Naegele and M. Bundschus
+ *
+ * This file is part of the Java Data Mining Package (JDMP).
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * JDMP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * JDMP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with JDMP; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package org.jdmp.complete;
+
+import junit.framework.TestSuite;
+
+public class AllTests extends TestSuite {
+
+	public static TestSuite suite() {
+		TestSuite suite = new TestSuite(AllTests.class.getName());
+		suite.addTest(org.ujmp.complete.AllTests.suite());
+		suite.addTest(org.jdmp.core.AllTests.suite());
+		suite.addTest(org.jdmp.ehcache.AllTests.suite());
+		suite.addTest(org.jdmp.gui.AllTests.suite());
+		suite.addTest(org.jdmp.jgroups.AllTests.suite());
+		suite.addTest(org.jdmp.libsvm.AllTests.suite());
+		suite.addTest(org.jdmp.mallet.AllTests.suite());
+		suite.addTest(org.jdmp.weka.AllTests.suite());
+		suite.addTest(org.jdmp.bsh.AllTests.suite());
+		suite.addTest(org.jdmp.lucene.AllTests.suite());
+		return suite;
+	}
+
+}
