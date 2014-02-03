8d06bf2d4b14c43906d85ef5bee9241ab937aac8
holger-arndt <Holger Arndt>
2014-02-03 17:13:36 +0000

    refactoring

--- jdmp-core/src/test/java/org/jdmp/core/AllTests.java
+++ jdmp-core/src/test/java/org/jdmp/core/AllTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008-2013 by Holger Arndt
+ * Copyright (C) 2008-2014 by Holger Arndt
  *
  * This file is part of the Java Data Mining Package (JDMP).
  * See the NOTICE file distributed with this work for additional
@@ -27,7 +27,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses( { TestScript.class, TestNaiveBayesClassifier.class, TestMLP.class,
+@Suite.SuiteClasses({ TestScript.class, TestNaiveBayesClassifier.class, TestMLP.class,
 		TestLinearRegression.class, TestRandomClassifier.class, TestConstantClassifier.class,
 		TestScript.class })
 public class AllTests {
